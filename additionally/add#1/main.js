347542043c215ae4d1bceb52af7e4972f8f821b6
Ilya <t4k.org@mail.ru>
2018-08-09 23:05:28 +0300

    add

--- additionally/add#1/main.js
+++ additionally/add#1/main.js
@@ -1,14 +1,19 @@
 let num = 33721;
+var arr = String(num).split('');
+var result = arr.reduce((a,b) => a * b);
+
+result = Math.pow(result, 3);
+
+let two;
+
+two = +(String(result).charAt(0) + String(result).charAt(1));
+
+let div = document.createElement('div');
+
+document.body.appendChild(div);
+div.innerHTML = two;
+
+console.log(two);
 
 
-
-function arrOne(a) {
-    var b = String(a);    
-    var arr = b.split('');
-    var result = arr.reduce((a,b) => a * b);
-    result = Math.pow(result, 3);
-    
-    console.log(result)
-}
-arrOne(num)
 
